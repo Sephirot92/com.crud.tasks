c15a461fe4667609e76945d9faf8baed416140cd
Łukasz Czapraga <lczapraga@gmail.com>
2019-12-04 22:35:59 +0100

    Task 23.4 - Created Email Text validation.

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -24,7 +24,7 @@ public class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "Test", "Tst Message");
+        Mail mail = new Mail("test@test.com", "", "Test", "Tst Message");
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
